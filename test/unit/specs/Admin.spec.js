c41498d5af461d567fd3b044500b1c63c5061590
developerlin123 <developerlin123@gmail.com>
2021-12-07 19:38:41 +0100

    update components rendering test

--- test/unit/specs/Admin.spec.js
+++ test/unit/specs/Admin.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount ,mount,createLocalVue} from '@vue/test-utils'
 import Admin from '../../../src/components/Pages/Admin'
+import LeftCatalog from '../../../src/components/Compos/LeftCatalog'
+import MiddleArticleList from '../../../src/components/Compos/MiddleArticleList'
 import store  from '@/vuex/store'
 
 import ElementUI from "element-ui"
@@ -17,12 +19,21 @@ describe('All components should be rendered:', () => {
     expect(wrapper.find('.el-header').text()).toBe("longlin_vuexdemo_CR(no U)D")
   })
 
+  it('MiddleArticleList compo should be rendered', () => {
+    expect(wrapper.findComponent(MiddleArticleList).exists()).toBe(true)
+  })
+
   it('MiddleArticleList compo should have create btn', () => {
     expect(wrapper.find('#createBtn').text()).toBe("Create Article")
+  })
+
+  it('LeftCatalog compo should be rendered', () => {
+    expect(wrapper.findComponent(LeftCatalog).exists()).toBe(true)
   })
 
   it('LeftCatalog compo should have create btn', () => {
     expect(wrapper.find('#tasteTypes').text()).toBe("Flavours")
   })
+
 
 })
