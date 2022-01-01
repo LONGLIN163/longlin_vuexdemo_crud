7231e2202e3959533e3503ad15562a79f2834127
developerlin123 <developerlin123@gmail.com>
2022-01-01 20:43:22 +0100

    update actions with es6 async syntax

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,24 +1,26 @@
 import JSONPath from "JSONPath"
 import Article from "../apis/Article";
 
-export const setArticleListAction = ({commit})=>{
-  Article.getArticleData()
-  .then(res=>{
+export const setArticleListAction = async ({commit})=>{
+  await Article.getArticleData()
+  .then(async res=>{
+
     if(res.status==200){
-      const objArr=JSONPath({
+      const objArr= await JSONPath({
         json:res.data.data,
         path:'$..[?(@.isFruit&&@.name)]'
       })
-        commit('GET_ARTICLELIST',objArr) 
-        commit('GET_TYPES') 
+
+      await commit('GET_ARTICLELIST',objArr) 
+      commit('GET_TYPES') 
     }
   }).catch(err=>{
       console.log(err)
   })
 }
 
-export const createArticleAction = ({dispatch},aobj) => {
-  Article.postArticleData(aobj)
+export const createArticleAction = async ({dispatch},aobj) => {
+  await Article.postArticleData(aobj)
   .then(res=>{
     if(res.status==201){
       dispatch('setArticleListAction')
@@ -28,13 +30,52 @@ export const createArticleAction = ({dispatch},aobj) => {
   })
 }
 
-export const delArticleAction = ({dispatch,commit},id) => {
-    Article.deleteArticleData(id).then(res=>{
+export const delArticleAction = async ({dispatch,commit},id) => {
+    await Article.deleteArticleData(id).then(async res=>{
         if(res.status==200){
-          dispatch('setArticleListAction')
+          await dispatch('setArticleListAction')
           commit('TOOGLE_REFRESHCOMPO',true)
         }
     }).catch(err=>{
         console.log(err)
     })
 }
+
+//*********************************************************
+// export const setArticleListAction = ({commit})=>{
+//   Article.getArticleData()
+//   .then(res=>{
+//     if(res.status==200){
+//       const objArr=JSONPath({
+//         json:res.data.data,
+//         path:'$..[?(@.isFruit&&@.name)]'
+//       })
+//         commit('GET_ARTICLELIST',objArr) 
+//         commit('GET_TYPES') 
+//     }
+//   }).catch(err=>{
+//       console.log(err)
+//   })
+// }
+
+// export const createArticleAction = ({dispatch},aobj) => {
+//   Article.postArticleData(aobj)
+//   .then(res=>{
+//     if(res.status==201){
+//       dispatch('setArticleListAction')
+//     }
+//   }).catch(err=>{
+//         console.log(err)
+//   })
+// }
+
+// export const delArticleAction = ({dispatch,commit},id) => {
+//     Article.deleteArticleData(id).then(res=>{
+//         if(res.status==200){
+//           dispatch('setArticleListAction')
+//           commit('TOOGLE_REFRESHCOMPO',true)
+//         }
+//     }).catch(err=>{
+//         console.log(err)
+//     })
+// }
